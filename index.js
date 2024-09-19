0331a449937ce3598d7a5dbaaca93b39f7fb25c9
gabriele-betuzzi <gabriele.betuzzi@promedital.it>
2024-09-19 17:49:19 +0200

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { getSteelcoChemData } from "./src/steelco-chem.service.js";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
